101ea87bc11ddf9c9b525ddcee25da64927838e6
p2well <p2well@outlook.com>
2022-06-25 14:02:21 +0200

    Print a Go proverb

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/p2well/go-dev-essentials/mascot"
+	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Welcome Gophers 😎")
 	fmt.Println(mascot.BestMascot())
+	fmt.Println(quote.Go())
 }
